05ddbf34d902aca01fa31eeee1ce0b91eb0c7d6c
Sebas <Sebas@10.100.32.156>
2024-08-09 10:42:02 -0600

    pruebas

--- appPruebaProyecto/src/apppruebaproyecto/AppPruebaProyecto.java
+++ appPruebaProyecto/src/apppruebaproyecto/AppPruebaProyecto.java
@@ -16,7 +16,7 @@ public class AppPruebaProyecto {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        //holis
+        //holissss
     }
     
 }
