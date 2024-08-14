efc6ddcd79a413384e0c398f9feb97dfc5f8c084
Davidcruzalf0510 <davidcruzalf05@gmail.com>
2024-08-14 11:31:15 -0600

    actu

--- appPruebaProyecto/src/apppruebaproyecto/AppPruebaProyecto.java
+++ appPruebaProyecto/src/apppruebaproyecto/AppPruebaProyecto.java
@@ -16,8 +16,8 @@ public class AppPruebaProyecto {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        //holasss 10
-        System.out.println("hola mundo");
+        
+        System.out.println("Hola Mundo");
     }
     
 }
